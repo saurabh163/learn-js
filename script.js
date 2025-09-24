edc0d656f7ffdc2b69bc12583490cbaebc7a8cf9
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-09-24 09:52:42 +0530

    tabel multiplication

--- script.js
+++ script.js
@@ -740,3 +740,15 @@ function reverscounting(){
 }
 
 reverscounting();
+
+// big maltipication problem
+
+function bigMultiplicationTable(num) {
+  
+    for (let i = 1; i <= 20; i++) {
+        console.log(num + " × " + i + " = " + (num * i));
+    }
+}
+
+bigMultiplicationTable(7);
+
