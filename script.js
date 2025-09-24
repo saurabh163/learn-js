9c3c2067c38395780f8400cbb2451939ffca1d1d
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-09-24 12:11:15 +0530

    first simple arr

--- script.js
+++ script.js
@@ -752,3 +752,23 @@ function bigMultiplicationTable(num) {
 
 bigMultiplicationTable(7);
 
+
+
+// array problem
+
+function findLargestNumber(numbers) {
+    let largest = numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+      
+        if (numbers[i] > largest) {
+            largest = numbers[i];
+        }
+    }
+
+    console.log("The largest number is: " + largest);
+}
+
+// Test arr
+findLargestNumber([12, 45, 7, 89, 23, 56]);
+
