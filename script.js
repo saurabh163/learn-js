719c92c62938ddbe276b011e9db235394cd65c1c
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-09-25 20:47:29 +0530

    divide problem

--- script.js
+++ script.js
@@ -895,8 +895,22 @@ for (let i = 1; i <= 5; i++) {
 
     if (i % 2 === 0) {
         console.log(i + " is Even");
-        
+
     } else {
         console.log(i + " is Odd");
     }
 }
+
+// forloop divide 
+
+for (let i = 1; i <= 10; i++) {
+
+    if (i % 3 === 0) {
+        console.log(i + " is divisible by 3");
+        
+    } else {
+        console.log(i + " is not divisible by 3");
+    }
+}
+
+
