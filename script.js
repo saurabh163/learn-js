aae4564ac4b9d53bacbea5c63f0c95299ef39bcf
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-09-26 12:36:31 +0530

    arr even number problem

--- script.js
+++ script.js
@@ -934,3 +934,17 @@ if (Age >= 18 && mark >= 50) {
     console.log("Student too young and failed");
 }
 
+
+
+//arr se even number print problem
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+console.log("Even numbers in the array:");
+
+for (let i = 0; i < numbers.length; i++) {
+  
+    if (numbers[i] % 2 === 0) {   
+        console.log(numbers[i]);
+    }
+}
