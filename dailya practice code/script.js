b5968e3739936f821557ad2ff6ff51da38bf66ab
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-04 19:33:11 +0530

    calculate discount

---
+++ dailya practice code/script.js
@@ -0,0 +1,38 @@
+// problem  01
+ 
+function calculateDiscount(amount) 
+{
+  if (amount <= 0) {
+    console.log("Invalid Amount");
+  } 
+
+  else if (amount < 500) {
+    console.log("No Discount");
+  }
+
+  else if (amount <= 1000) {
+    let discount = amount * 0.10;
+    console.log("10% Discount Applied");
+  }
+
+  else if (amount <= 5000) {
+    let discount = amount * 0.20;
+    console.log("20% Discount Applied");
+  }
+
+  else {
+    let discount = amount * 0.30;
+    console.log("30% Discount Applied");
+  }
+}
+
+  // test function//
+calculateDiscount(-200);  
+
+calculateDiscount(300);  
+
+calculateDiscount(800);  
+
+calculateDiscount(2000);  
+
+calculateDiscount(6000);
