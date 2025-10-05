37d1bcb412f3f948eca97ae57b06aeec8ac8f780
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-05 23:18:47 +0530

    alert betton text

--- dailya practice code/oct_5_the_dom/script.js
+++ dailya practice code/oct_5_the_dom/script.js
@@ -13,3 +13,11 @@ document.getElementById("bgBtn").addEventListener("click", function() {
 
   document.body.style.backgroundColor = "lightpink";  /// first i click a boton  after chnge the color bg
 });
+
+
+// alert booton 
+
+document.getElementById("alertBtn").addEventListener("click", function() {
+
+  alert("Hello guy's Welcome to DOM practice....");
+});
