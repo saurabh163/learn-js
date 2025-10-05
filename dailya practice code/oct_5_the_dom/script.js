bc33934279d31fe41004debfd720ec35005ad3c4
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-05 23:14:47 +0530

    bg color change

--- dailya practice code/oct_5_the_dom/script.js
+++ dailya practice code/oct_5_the_dom/script.js
@@ -1,7 +1,15 @@
-
+// chnge text
 
 document.getElementById("changeBtn").addEventListener("click", function() {
 
   document.getElementById("myPara").innerText = "Welcome to JavaScript DOM..!"; // change paragraph text when button clicke to this
 
+});
+
+
+// bg colour 
+
+document.getElementById("bgBtn").addEventListener("click", function() {
+
+  document.body.style.backgroundColor = "lightpink";  /// first i click a boton  after chnge the color bg
 });
