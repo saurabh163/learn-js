2055a55e14fa766f767374ab09de43c1393d191d
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-05 23:07:07 +0530

    pargraph chnge with dom

---
+++ dailya practice code/oct_5_the_dom/script.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("changeBtn").addEventListener("click", function() {
+
+  document.getElementById("myPara").innerText = "Welcome to JavaScript DOM..!"; // change paragraph text when button clicke to this
+
+});
