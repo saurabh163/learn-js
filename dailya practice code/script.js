1403321a9f9d36d87cb5b5673c202856d80cd419
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-12 23:15:22 +0530

    for loop even number loop

--- dailya practice code/script.js
+++ dailya practice code/script.js
@@ -738,3 +738,14 @@ for (let i = 1; i <= 10; i++) {
 }
 
 console.log("Sum =", sum);
+
+
+
+//problem 44 //// even number print with for loop
+
+for (let i = 1; i <= 20; i++) {
+
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
