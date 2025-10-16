91fe87fe9bd8edcc1fe0d2c33ab12ca8762f42d5
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-16 22:49:12 +0530

    two variable

--- dailya practice code/script.js
+++ dailya practice code/script.js
@@ -1239,3 +1239,31 @@ if (NuM > 0) {
 } else {
     console.log("Negative number");
 }
+
+
+
+
+
+// problem 70 //// tiket booking problem 
+
+
+let ticketPrice = 250;
+let tickets = 10;
+
+let Total = ticketPrice * tickets;
+
+if (ticketPrice <= 0 || tickets <= 0) {
+  console.log("Invalid Input");
+}  
+
+else if (total > 2000) {
+  let discount = total * 0.10;
+  let finalBill = total - discount;
+  console.log("You got 10% Discount");
+  console.log("Final Bill:");
+} 
+
+else {
+  console.log("No Discount, Please Pay Full Amount");
+  console.log("Total Bill:");
+}
