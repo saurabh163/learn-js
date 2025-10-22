04096a6c330d1adb9651a93bda7cf9035ccffc58
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-22 16:47:52 +0530

    Dubble click event

--- dailya practice code/oct_5_the_dom/dom practice/Oct_22_Event_handling/script.js
+++ dailya practice code/oct_5_the_dom/dom practice/Oct_22_Event_handling/script.js
@@ -7,3 +7,12 @@ const msg = document.getElementById("msg");
 btn.addEventListener("click", function() {
   msg.textContent = "Hello Bro I'm lernning event handling ";
 });
+
+
+// box color change //
+
+const box = document.getElementById("box");
+
+box.addEventListener("dblclick", function() {
+  box.style.backgroundColor = "yellow";
+});
