d42accfb80ee5953dfe3377dac52ff596c060d59
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-22 16:41:27 +0530

    click event

---
+++ dailya practice code/oct_5_the_dom/dom practice/Oct_22_Event_handling/script.js
@@ -0,0 +1,9 @@
+
+// massage show //
+
+const btn = document.getElementById("btn");
+const msg = document.getElementById("msg");
+
+btn.addEventListener("click", function() {
+  msg.textContent = "Hello Bro I'm lernning event handling ";
+});
