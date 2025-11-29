7a79164fbada285fe2ee7b9ad2736a9f1d21b3c7
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-11-29 22:13:47 +0530

    arry number exite or not

--- 29_nov_2025/script.js
+++ 29_nov_2025/script.js
@@ -10,3 +10,16 @@ function isEmpty(arr) {
 
 console.log(isEmpty([]));        
 console.log(isEmpty([10, 20]));  
+
+// problem 02 //
+
+function findNumber(arr, num) {
+  if (arr.includes(num)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(findNumber([1,2,3], 2)); 
+console.log(findNumber([1,2,3], 5)); 
