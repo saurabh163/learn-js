faf57a5a7d9a2cf7b47c8bfec1a5fb34718bcb0e
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-11-29 22:12:58 +0530

    js emty fuction true false check

---
+++ 29_nov_2025/script.js
@@ -0,0 +1,12 @@
+//problem 01 //
+
+function isEmpty(arr) {
+  if (arr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isEmpty([]));        
+console.log(isEmpty([10, 20]));  
