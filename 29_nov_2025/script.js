ec7d33c8b4ec59b2591fc789fdf0ddba9354a8a0
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-11-30 23:47:15 +0530

    arry forloop

--- 29_nov_2025/script.js
+++ 29_nov_2025/script.js
@@ -162,3 +162,12 @@ else {
         console.log(i + " is Odd");
     }
 }
+
+
+// problem 12 //
+
+let value = [10, 20, 30, 40, 50];
+
+for (let i = value.length - 1; i >= 0; i--) {
+    console.log(value[i]);
+}
