403674bb8449720e596b3408432ff721bbe23ab6
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-12-01 22:09:51 +0530

    arry forloop

--- 29_nov_2025/script.js
+++ 29_nov_2025/script.js
@@ -302,3 +302,12 @@ if (userInput === correctPassword) {
 } else {
   console.log("Incorrect Password ");
 }
+
+
+// problem 25 //
+
+let numbers = [10, 20, 30, 40, 50];
+
+for (let i = numbers.length - 1; i >= 0; i--) {
+    console.log(numbers[i]);
+}
