5bf357d1787a2bc48f695092b99b20dfd401c626
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-12-01 22:13:56 +0530

    function check even odd

--- 29_nov_2025/script.js
+++ 29_nov_2025/script.js
@@ -352,3 +352,14 @@ function showAlert() {
   alert("Form submitted successfully!");
   return true;
 }
+
+//problem 29 //
+
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    console.log(num + " is Even");
+  } else {
+    console.log(num + " is Odd");
+  }
+}
+checkEvenOdd(9);
