5b52ea10ca4610b612089e739df5ccdc84fb057b
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-12-21 22:19:10 +0530

    live input preview using DOM

--- 21_Dec_25 dom/dom.js
+++ 21_Dec_25 dom/dom.js
@@ -6,3 +6,8 @@ document.getElementById("toggle").onclick = function () {
   let box = document.getElementById("box");
   box.style.display = box.style.display === "none" ? "block" : "none";
 };
+
+document.getElementById("input").addEventListener("input", function () {
+  document.getElementById("preview").innerText = this.value;
+});
+
